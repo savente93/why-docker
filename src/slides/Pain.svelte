479d04cb5fda3f64309302779009fc313b160fe6
Sam Vente <savente93@gmail.com>
2023-09-08 14:58:58 +0200

    tick

--- src/slides/Pain.svelte
+++ src/slides/Pain.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-    import Slide from "../lib/Slide.svelte";
+  import Slide from "../lib/Slide.svelte";
 </script>
 
 <Slide>
-<h3 style="text-transform :none;"> Jokes aside I think we've all been in this place before: </h3>
-<div class="fragment fade-up slow-animation">
-<h2> ¯\_(ツ)_/¯ </h2>
-<h3> It works on my machine!</h3>
-</div>
+  <h3 style="text-transform :none;">
+    Jokes aside I think we've all been in this place before:
+  </h3>
+  <div class="fragment fade-up slow-animation">
+    <h2>¯\_(ツ)_/¯</h2>
+    <h3>It works on my machine!</h3>
+  </div>
 </Slide>
 
 <style>
-.slow-animation {
- transition: all 1s ease;   
-}
-img {
-  margin: auto
-}
-
+  .slow-animation {
+    transition: all 1s ease;
+  }
 </style>
