082e3723fbcaf63559272cf38043aa4f60898dca
Sam Vente <savente93@gmail.com>
2023-09-12 20:20:00 +0200

    improve end screen with links

--- src/slides/Conclusions.svelte
+++ src/slides/Conclusions.svelte
@@ -3,5 +3,11 @@
 </script>
 
 <Slide>
-  <h2>Thank you!</h2>
+  <h3> For those interested: </h3>
+  <ul>
+  <li class="fragment fade-in">The slides for this presentation are available at <br /><a href="why-docker.howtoai.fyi">why-docker.howtoai.fyi</a></li>
+  <li class="fragment fade-in">And the image at <a href="https://hub.docker.com/r/savente/why-docker">https://hub.docker.com/r/savente/why-docker</a></li>
+  </ul>
+  <h2 class="fragment fade-in">Thank you!</h2>
+
 </Slide>
