4131192ea6647a336f4ac60ea55d69b84b84b478
Sam Vente <savente93@gmail.com>
2023-09-12 14:42:49 +0200

    introduce intentional spelling mistake

--- src/slides/WhatIsDocker.svelte
+++ src/slides/WhatIsDocker.svelte
@@ -7,6 +7,6 @@
 <h2 class="fragment fade-in">What is Docker?</h2>
 <p class="fragment fade-in">It's a box you can put code in.</p>
 <p class="fragment fade-in">And ship across the world.</p>
-<p class="fragment fade-in">Just like physical ship containers.</p>
+<p class="fragment fade-in">Just like fysical ship containers.</p>
 <p class="fragment fade-in">(Get it?)</p>
 </Slide>
